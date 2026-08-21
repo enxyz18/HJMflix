5907a035d1f49132fc2700c0ae1014c52269e144
enxyz18 <enxyz18@gmail.com>
2026-08-21 12:53:18 +0800

    fix disney plus provider, add dedicated platform pages & pagination

--- api/movies.js
+++ api/movies.js
@@ -14,12 +14,12 @@ export default async function handler(req, res) {
     return res.status(500).json({ error: "TMDB_API_KEY belum ditetapkan di Vercel!" });
   }
 
-  const { type, query, category } = req.query;
+  const { type, query, category, page = 1 } = req.query;
 
-  // 1. Carian Pintar (Filem, TV Series & Pelakon)
+  // 1. Carian Pintar dengan Paginasi
   if (type === 'search' && query) {
     try {
-      const searchUrl = `https://api.themoviedb.org/3/search/multi?api_key=${API_KEY}&query=${encodeURIComponent(query)}`;
+      const searchUrl = `https://api.themoviedb.org/3/search/multi?api_key=${API_KEY}&query=${encodeURIComponent(query)}&page=${page}`;
       const response = await fetch(searchUrl);
       const data = await response.json();
 
@@ -39,45 +39,47 @@ export default async function handler(req, res) {
         }
       }
 
-      // Buang item duplikasi berdasarkan ID
       const uniqueResults = Array.from(new Map(combinedResults.map(m => [m.id, m])).values());
 
-      return res.status(200).json({ results: uniqueResults });
+      return res.status(200).json({ 
+        results: uniqueResults, 
+        page: data.page || 1, 
+        total_pages: data.total_pages || 1 
+      });
     } catch (error) {
-      return res.status(500).json({ error: "Gagal memproses carian pelakon/tajuk" });
+      return res.status(500).json({ error: "Gagal memproses carian" });
     }
   } 
 
-  // 2. Kategori Platform
-  // TMDB Provider ID: Netflix=8, Prime=119, Disney+=337, HBO Max=1899 (Region: MY/US)
+  // 2. Kategori Platform dengan Paginasi
   let url = '';
   switch (category) {
     case 'netflix_movies':
-      url = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_watch_providers=8&watch_region=MY&sort_by=popularity.desc`;
+      url = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_watch_providers=8&watch_region=MY&sort_by=popularity.desc&page=${page}`;
       break;
     case 'netflix_tv':
-      url = `https://api.themoviedb.org/3/discover/tv?api_key=${API_KEY}&with_watch_providers=8&watch_region=MY&sort_by=popularity.desc`;
+      url = `https://api.themoviedb.org/3/discover/tv?api_key=${API_KEY}&with_watch_providers=8&watch_region=MY&sort_by=popularity.desc&page=${page}`;
       break;
     case 'prime_movies':
-      url = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_watch_providers=119&watch_region=MY&sort_by=popularity.desc`;
+      url = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_watch_providers=119&watch_region=MY&sort_by=popularity.desc&page=${page}`;
       break;
     case 'prime_tv':
-      url = `https://api.themoviedb.org/3/discover/tv?api_key=${API_KEY}&with_watch_providers=119&watch_region=MY&sort_by=popularity.desc`;
+      url = `https://api.themoviedb.org/3/discover/tv?api_key=${API_KEY}&with_watch_providers=119&watch_region=MY&sort_by=popularity.desc&page=${page}`;
       break;
     case 'disney_movies':
-      url = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_watch_providers=337&watch_region=MY&sort_by=popularity.desc`;
+      url = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_watch_providers=337|1229&sort_by=popularity.desc&page=${page}`;
       break;
     case 'disney_tv':
-      url = `https://api.themoviedb.org/3/discover/tv?api_key=${API_KEY}&with_watch_providers=337&watch_region=MY&sort_by=popularity.desc`;
+      url = `https://api.themoviedb.org/3/discover/tv?api_key=${API_KEY}&with_watch_providers=337|1229&sort_by=popularity.desc&page=${page}`;
       break;
     case 'hbo_movies':
-      url = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_watch_providers=1899|384&watch_region=US&sort_by=popularity.desc`;
+      url = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_watch_providers=1899|384&sort_by=popularity.desc&page=${page}`;
       break;
     case 'hbo_tv':
-      url = `https://api.themoviedb.org/3/discover/tv?api_key=${API_KEY}&with_watch_providers=1899|384&watch_region=US&sort_by=popularity.desc`;
+      url = `https://api.themoviedb.org/3/discover/tv?api_key=${API_KEY}&with_watch_providers=1899|384&sort_by=popularity.desc&page=${page}`;
       break;
     default:
-      url = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_watch_providers=8&watch_region=MY&sort_by=popularity.desc`;
+      url = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_watch_providers=8&watch_region=MY&sort_by=popularity.desc&page=${page}`;
       break;
   }
 
